6f9c7e2c393f8abf8b2bcccec4ace91fac3d6fdc
SorinMac <97994615+SorinMac@users.noreply.github.com>
2022-10-28 10:45:30 -0400

    Update index.php

--- index.php
+++ index.php
@@ -19,39 +19,31 @@
         </style>
     </head>
     <body>
-
     <!-- Logo to for the website-->
-
     <header>
         <center><img src="https://lh3.googleusercontent.com/R9mBkfrGZ7arGjewA9RXfo-Y6Miw-50wTVs8uoTtFLLjjZqkv1BRQZeXuWMQu9NIlIR7Qh2O3dNcgRd-qMMW-bVRCb_5KE-qBW2FIceB7vcdY_egVWyEb8AYx6CYLvhU_PQGjxtkMcg=w2400"; width = "100%"; height = 300px></center>
         
     </header>
-
     <!-- Will become links to navigate the website-->
-
     <main>
         <br>
         <center><table border=0></center>
         <table>
             <tr>
-                <th>Product</th>
-                <th>Sales</th>
-                <th>Login</th>
-                <th>Contact</th>
+                <th><a class= "active" href="product.php"> Product</a></th>
+                <th><a class= "active" href="sales.php"> Sales</a></th>
+                <th><a class= "active" href="login.php"> Login</a></th>
+                <th><a class= "active" href="contact.php"> Contact</a></th>
             </tr>
         </table>
         <br>
         
         <!-- Disclaimer for the website-->
-
         <center><h1> Disclaimer </h1></center>
-
         Please read the disclaimer below before using the Stellar Sneakers website operated by Maeve Lonergan, 
         Veronica Longley, Sorin Macaluso, Lillian McPadden.<br>
-
         <br>The contents of the website are the intellectual property of Sorin Macaluso. You are not allowed to<br>
         reuse, republish, or reprint any of the contents on this website without written consent.<br>
-
         <br>All posted information posted on Stellar Sneakers is used for the company so that we can insure that our customers have a<br>
         comfortable and easy experience on the website. We are committed to keeping the website up to date and accurate.<br>
         If anything is found to be incorrect or out of date please notify a IT representative.<br>
@@ -59,11 +51,8 @@
         <br>We are also not liable for any loss as a result of any inaccuracies or incompleteness. Or any problems that occurred<br>
         because of a disruption in the internet. Stellar Sneakers has no liability for any loss related to the data on this website.<br>
         <br>
-
         <!-- A small section descibing the company-->
-
         <center><h1> About Us</h1></center>
-
         Stellar Sneakers is a company that will allow anyone to sell their designer shoes. We will also be able to accurately<br> 
         identify any shoes that are replicas of the original shoe so that the customer is reassured that the product they are<br>
         buying is indeed authentic.<br>
@@ -72,14 +61,10 @@
         can use our patented checking process ensure that the shoes are real. Then when a coustomer is looking for a shoe, they can<br>
         purchase their desiered shoe from our website.
     </main>
-
     <br>
-
     <!-- Copy right-->
-
     <footer>
         <center><small> &copy; Maeve Lonergan, Veronica Longley, Sorin Macaluso, Lillian McPadden 2022</small></center>
     </footer>
     </body>
-
 </html>
