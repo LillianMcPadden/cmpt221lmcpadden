9e126c29c3408c9e56f10a2a70780e139e15b18e
SorinMac <97994615+SorinMac@users.noreply.github.com>
2022-11-03 12:31:44 -0400

    Create user.php

---
+++ user.php
@@ -0,0 +1,60 @@
+<!DOCTYPE html>
+<!--    t5homepage.html   Homepage
+    09/21/2022 t5 Original Program
+-->
+<html lang="en">
+
+    <head>
+        <title>
+            Stellar Sneakers Homepage
+        </title>
+        <meta charset="utf-8">
+        <style>
+            * {background-color: rgb(255, 255, 255);}
+            h1 { color : black; font-family: Papyrus;font-size: 45px;}
+            h3 { color:rgb(220, 26, 34);font-size: 25px;font-family: Monaco;}
+            header {background-color:rgb(220, 26, 34);}
+            footer {background-color:rgb(245, 255, 245);}
+            table {border-collapse: separate; border-spacing: 150px 0;}
+        </style>
+    </head>
+    <body>
+    <!-- Logo to for the website-->
+    <header>
+        <center><img src="https://lh3.googleusercontent.com/R9mBkfrGZ7arGjewA9RXfo-Y6Miw-50wTVs8uoTtFLLjjZqkv1BRQZeXuWMQu9NIlIR7Qh2O3dNcgRd-qMMW-bVRCb_5KE-qBW2FIceB7vcdY_egVWyEb8AYx6CYLvhU_PQGjxtkMcg=w2400"; width = "100%"; height = 300px></center>
+        
+    </header>
+    <!-- Will become links to navigate the website-->
+    <main>
+        <br>
+        <center><table border=0></center>
+        <table>
+            <tr>
+                <th><a class= "active" href="product.php"> Product</a></th>
+                <th><a class= "active" href="sales.php"> Sales</a></th>
+                <th><a class= "active" href="login.php"> Login</a></th>
+                <th><a class= "active" href="contact.php"> Contact</a></th>
+                <th><a class= "active" href="admin.php"> Admin</a></th>
+                <th><a class= "active" href="suppliers.php"> Suppliers</a></th>
+                <th><a class= "active" href="admin.php"> Admin</a></th>
+            </tr>
+        </table>
+        <br>
+        
+        <!-- start of the new code -->
+        <p style = "color:rgb(220, 26, 34)"> This is all of the companies infromation if you have any questions <br>
+        or concers with the company. </p>
+        <h3> Coustomer Service Number </h3>
+        <p> 000-000-0000 </p>
+        <h3> Coustomer Service Email </h3>
+        <p> <a href="mailto: sorin.macaluso1@marist.edu"> Contact Us</a> </p>
+        <p style = "color:rgb(220, 26, 34)"> If you have any question please feel free to email or call.</p>
+        
+    </main>
+    <br>
+    <!-- Copy right-->
+    <footer>
+        <center><small> &copy; Maeve Lonergan, Veronica Longley, Sorin Macaluso, Lillian McPadden 2022</small></center>
+    </footer>
+    </body>
+</html>
