29e4b76aedfa87407e5a0730fc513c817ae6ef6d
LillianMcPadden <89875986+LillianMcPadden@users.noreply.github.com>
2022-11-03 21:25:22 -0400

    Update product.php

--- product.php
+++ product.php
@@ -30,17 +30,44 @@
         <center><table border=0></center>
         <table>
             <tr>
+                <th><a class= "active" href="product.php"> Product</a></th>
                 <th><a class= "active" href="sales.php"> Sales</a></th>
                 <th><a class= "active" href="login.php"> Login</a></th>
                 <th><a class= "active" href="contact.php"> Contact</a></th>
-                <th><a class= "active" href="sales.php"> Suppliers</a></th>
                 <th><a class= "active" href="admin.php"> Admin</a></th>
+                <th><a class= "active" href="suppliers.php"> Suppliers</a></th>
             </tr>
         </table>
         <br>
-        
-        <!-- start of the new code -->
-        
+        <?php
+        define("FILE_AUTHOR", "Lillian McPadden");
+        echo " <p><b> Connect to site_db: </b>";
+        require "t5connect.php";
+        echo "<h3>Explain the Product Table </h3>";
+        $q = "Explain t5_product";
+        echo " <table> <tr> <th> Field </th> <th> Type </th> <th> Null </th> <th> Key </th> <th> Default </th> ";
+        $r = mysqli_query($dbc, $q);
+        if($r ){
+            while ($row= mysqli_fetch_array($r, MYSQLI_NUM))
+            {
+                echo "<br><tr><td>". $row[0]. "</td><td> ". $row[1]. "</td><td> ". $row[2]. "</td><td> ". $row[3]. " </td><td>". $row['4']. "</td></tr>";
+            }
+        }
+        echo "</table>";
+
+        echo "<br><br><h3>Display the Product Table </h3>";
+        $q = "Select * from t5_product";
+        $r = mysqli_query($dbc, $q);
+
+        echo " <table> <tr> <th> product_ID </th> <th> our_price </th> <th> color </th> <th> quantity </th> <th> shoe_cond  </th> <th> size </th><th> brand </th><th> style_num </th><th> listing_price </th><th> deleted </th>";
+        if($r ){
+            while ($row= mysqli_fetch_array($r, MYSQLI_NUM))
+            {
+                echo "<br><tr><td>". $row[0] . "</td><td>". $row[1]. "</td><td>". $row[2]. "</td><td>". $row[3] . "</td><td>". $row[4]. "</td><td>". $row[5]. "</td><td> ". $row[6]. "</td><td>". $row[7]. "</td><td>". $row[8]. "</td></tr>";
+            }
+        }
+        echo "</table>";
+        ?>
     </main>
     <br>
     <!-- Copy right-->
