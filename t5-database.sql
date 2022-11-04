f50d5d0c0f067d9ceed158c6ed9c74727820ce02
LillianMcPadden <89875986+LillianMcPadden@users.noreply.github.com>
2022-11-03 22:44:23 -0400

    Update t5-database.sql

--- t5-database.sql
+++ t5-database.sql
@@ -11,9 +11,9 @@ CREATE TABLE IF NOT EXISTS t5_product(
     size DECIMAL(2,1),
     brand enum('Nike', 'Jordan', 'Adidas', 'Converse', 'Reebok', 'New Balance', 'Vans', 'Puma', 'Yeezy'),
     style_num INT,
-    listing_price DECIMAL(6,2),
-    deleted enum("y", "n"),
-    supplier varchar(100));
+    listing_price DECIMAL(6,2));
+    deleted enum("y", "n");
+    supplier varchar(100);
 
 DROP TABLE IF EXISTS t5_supplier;
 -- Supplier Table
@@ -23,8 +23,8 @@ CREATE TABLE IF NOT EXISTS t5_supplier (
     supAddress VARCHAR(100),
     number_of_sales INT,
     supEmail VARCHAR(100),
-    supPhone_num CHAR(10),
-    deleted enum("y", "n"));
+    supPhone_num CHAR(10));
+    deleted enum("y", "n");
 
 DROP TABLE IF EXISTS t5_customer;
 -- Customer Table
@@ -33,8 +33,8 @@ CREATE TABLE IF NOT EXISTS t5_customer(
     last_name VARCHAR(100),
     first_name VARCHAR(100),
     cus_address VARCHAR(100),
-    cus_email VARCHAR(100),
-    deleted ENUM("y", "n"));
+    cus_email VARCHAR(100));
+    deleted enum("y", "n");
 
 DROP TABLE IF EXISTS t5_user;
 -- User Table
@@ -43,11 +43,11 @@ CREATE TABLE IF NOT EXISTS t5_user(
     passwrd VARCHAR(100),
     passwrd_reset DATE,
     title Enum('Stakeholder', 'Employee', 'Admin/IT', 'Customer'),
-    monthly_report BLOB,
-    deleted enum("y", "n"),
-    email varchar(100),
-    lastchanged date,
-    password_type enum("hashed", "text"));
+    monthly_report BLOB);
+    deleted enum("y", "n");
+    email varchar(100);
+    lastchanged date;
+    password_type enum("hashed", "text");
 
 DROP TABLE IF EXISTS t5_orders;
 -- Orders Table
@@ -55,10 +55,9 @@ CREATE TABLE IF NOT EXISTS t5_orders(
     order_num INT PRIMARY KEY,
     customer_id INT,
     product_id INT,
-    order_status Enum('Processing','Shipped', 'Delivered'),
-    deleted enum("y", "n"));
-    
-    
+    order_status Enum('Processing','Shipped', 'Delivered'));
+    deleted enum("y", "n");
+
 insert into t5_supplier VALUES (111, 'John Adams', '1519 Homes Ave', 14, 'jadams@gmail.com', '2420908954', 'n');
 insert into t5_supplier VALUES (112, 'Josh Peters', '35 Center St.', 55, 'joshpeters@gmail.com', '3145647227', 'n');
 insert into t5_supplier VALUES (113, 'Valerie Hanks', '55 Rays Ct. ', 30, 'valhanks@yahoo.com', '4139059090', 'n');
@@ -71,3 +70,10 @@ INSERT INTO t5_user VALUES('SA85h4', 'heJ78ke', '2023-01-11', 'Admin/IT', 'N/A',
 INSERT INTO t5_user VALUES('89JK76', '1985hwu', '2023-01-13', 'Stakeholder', 'N/A', 'n', 'susanbridges9@gmail.com', '2022-08-17', 'hashed');
 INSERT INTO t5_user VALUES('73WX8f', '00yu54#', '2023-02-01', 'Customer', 'N/A', 'n', 'mikemarsh@gmail.com', '2022-06-05', 'hashed');
 INSERT INTO t5_user VALUES('SW5862', '64Jjuq3', '2023-12-05', 'Employee', 'N/A', 'n', 'katebrown@gmail.com', '2022-10-05', 'hashed');
+
+INSERT INTO t5_product VALUES(1, 50, 'black', 2, 'Grade A', 7, 'Nike', 109, 40, 'y');
+INSERT INTO t5_product VALUES(2, 60, 'gray', 3, 'Grade B', 8, 'Jordan', 210, 50, 'n');
+INSERT INTO t5_product VALUES(3, 70, 'red', 1, 'Grade C', 9, 'Adidas', 321, 60, 'y');
+INSERT INTO t5_product VALUES(4, 80, 'orange', 4, 'Grade D', 10, 'Yeezy', 432, 70, 'n');
+INSERT INTO t5_product VALUES(5, 90, 'blue', 2, 'Grade F', 11, 'Vans', 543, 80, 'y');
+INSERT INTO t5_product VALUES(6, 100, 'beige', 5, 'Grade A', 12.5, 'Converse', 654, 90, 'n');
