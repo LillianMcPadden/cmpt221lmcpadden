5549cbfe919c7ed244120ed36e4d07f1b6f0eeae
LillianMcPadden <89875986+LillianMcPadden@users.noreply.github.com>
2022-11-15 14:02:38 -0500

    added buttons and fixed table

--- product.php
+++ product.php
@@ -15,7 +15,6 @@
             h3 { color:rgb(220, 26, 34);font-size: 25px;font-family: Monaco;}
             header {background-color:rgb(220, 26, 34);}
             footer {background-color:rgb(245, 255, 245);}
-            table {border-collapse: separate; border-spacing: 150px 0;}
         </style>
     </head>
     <body>
@@ -35,44 +34,63 @@
                 <th><a class= "active" href="login.php"> Login</a></th>
                 <th><a class= "active" href="contact.php"> Contact</a></th>
                 <th><a class= "active" href="admin.php"> Admin</a></th>
-                <th><a class= "active" href="suppliers.php"> Suppliers</a></th>
+                <th><a class= "active" href="sales.php"> Suppliers</a></th>
             </tr>
         </table>
         <br>
         <?php
         define("FILE_AUTHOR", "Lillian McPadden");
         echo " <p><b> Connect to site_db: </b>";
-        require "t5connect.php";
-        echo "<h3>Explain the Product Table </h3>";
-        $q = "Explain t5_product";
-        echo " <table> <tr> <th> Field </th> <th> Type </th> <th> Null </th> <th> Key </th> <th> Default </th> ";
-        $r = mysqli_query($dbc, $q);
-        if($r ){
-            while ($row= mysqli_fetch_array($r, MYSQLI_NUM))
-            {
-                echo "<br><tr><td>". $row[0]. "</td><td> ". $row[1]. "</td><td> ". $row[2]. "</td><td> ". $row[3]. " </td><td>". $row['4']. "</td></tr>";
-            }
+        require "connect_db.php";
+
+        if(ISSET($_POST['sort'])){
+            $sort_type = " ORDER BY ". $_POST['sort'];
         }
-        echo "</table>";
+        else {
+            $sort_type = " ";
+        }
+        if(ISSET($_POST['direction'])){
+            $dir = $_POST['direction'];
+        }
+        else {
+            $dir = " ";
+        }
 
         echo "<br><br><h3>Display the Product Table </h3>";
-        $q = "Select * from t5_product";
+        $q = "Select * from t5_product $sort_type $dir";
         $r = mysqli_query($dbc, $q);
 
-        echo " <table> <tr> <th> product_ID </th> <th> our_price </th> <th> color </th> <th> quantity </th> <th> shoe_cond  </th> <th> size </th><th> brand </th><th> style_num </th><th> listing_price </th><th> deleted </th>";
+        echo " <table border = '2' cellpadding = '2' border-spacing = '0px 0'> <tr><th> product_ID </th><th> our_price </th><th> color </th><th> quantity </th><th> shoe_cond  </th> <th> size </th><th> brand </th><th> style_num </th><th> listing_price </th><th> deleted </th></tr>";
         if($r ){
             while ($row= mysqli_fetch_array($r, MYSQLI_NUM))
             {
-                echo "<br><tr><td>". $row[0] . "</td><td>". $row[1]. "</td><td>". $row[2]. "</td><td>". $row[3] . "</td><td>". $row[4]. "</td><td>". $row[5]. "</td><td> ". $row[6]. "</td><td>". $row[7]. "</td><td>". $row[8]. "</td></tr>";
+                echo "<tr><td>". $row[0] . "</td><td>". $row[1]. "</td><td>". $row[2]. "</td><td>". $row[3] . "</td><td>". $row[4]. "</td><td>". $row[5]. "</td><td> ". $row[6]. "</td><td>". $row[7]. "</td><td>". $row[8]. "</td></tr>";
             }
         }
         echo "</table>";
+
+        echo "<form action = '' method = 'POST'>";
+        echo "<br> <input type = 'submit' value = 'Sort it!' style = 'background-color:rgb(220, 26, 34); color:white;'>";
+        echo "<input type = 'radio' name = 'sort' value = 'product_id'>    ID";
+        echo "<input type = 'radio' name = 'sort' value = 'our_price'>    Our Price";
+        echo "<input type = 'radio' name = 'sort' value = 'color'>    Color";
+        echo "<input type = 'radio' name = 'sort' value = 'quantity'>    Quantity";
+        echo "<input type = 'radio' name = 'sort' value = 'shoe_cond'>    Shoe Condition";
+        echo "<input type = 'radio' name = 'sort' value = 'size'>    Size";
+        echo "<input type = 'radio' name = 'sort' value = 'brand'>    Brand";
+        echo "<input type = 'radio' name = 'sort' value = 'style_num'>    Style Number";
+        echo "<input type = 'radio' name = 'sort' value = 'listing_price'>    Listing Price";
+        echo "<input type = 'radio' name = 'sort' value = 'deleted'>    Deleted";
+        echo "<br><input type = 'radio' name = 'direction' value = 'ASC'>    Ascending";
+        echo "<input type = 'radio' name = 'direction' value = 'DESC'>    Descending";
+        echo "</form>";
+
         ?>
     </main>
     <br>
     <!-- Copy right-->
     <footer>
-        <center><small> &copy; Maeve Lonergan, Veronica Longley, Sorin Macaluso, Lillian McPadden 2022</small></center>
+        <center><small> &copy; Lillian McPadden 2022</small></center>
     </footer>
     </body>
 </html>
