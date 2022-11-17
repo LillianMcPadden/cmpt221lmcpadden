92c5f76bfe390742346efc88933f62f346bfc694
MaeveLonergan <98499704+MaeveLonergan@users.noreply.github.com>
2022-11-17 14:21:50 -0500

    Update and rename File_Author.php to file_author.php

--- File_Author.php
+++
@@ -1,7 +0,0 @@
-<?php
-    echo "<footer>
-        <hr>
-        <p style ='text-align:center'>
-        <small> (c) S.Macaluso, 2022</small>
-        </footer>"
-?>
---
+++ file_author.php
@@ -0,0 +1,3 @@
+<?php
+echo "<center><small> &copy; $FILE_AUTHOR 2022</small></center>"
+?>
