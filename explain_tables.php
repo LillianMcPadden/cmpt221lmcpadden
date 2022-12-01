5047c4c20985733aae4ef3a4d8623bb2f001e032
MaeveLonergan <98499704+MaeveLonergan@users.noreply.github.com>
2022-12-01 18:09:45 -0500

    Update explain_tables.php

--- explain_tables.php
+++ explain_tables.php
@@ -15,7 +15,12 @@
             h3 { color:rgb(220, 26, 34);font-size: 25px;font-family: Monaco;}
             header {background-color:rgb(220, 26, 34);}
             footer {background-color:rgb(245, 255, 245);}
-           th {font-family: Papyrus;}
+           th {font-family: Papyrus; padding: 5px 5px 5px 5px; font-weight:2300; font-size: 25px;}
+            td {padding: 5px 5px 5px 5px;font-size: 25px;} 
+            form { font-family: Monaco;font-size: 25px;}
+           .tableoutline{background-color:rgb(220, 26, 34);}
+           .tableheader{background:rgb(249, 208, 208 );}
+           .redcolor{color:rgb(220, 26, 34);}
         </style>
     </head>
     <body>
@@ -45,69 +50,90 @@
            
         <?php
             $FILE_AUTHOR = "Maeve Lonergan";
-            echo " <p><b> Connect to site_db: </b>";
+          
             require "../connect_db.php";
-            echo "<hr>";
-            echo "<h3> Customers table explained </h3>";
-            echo "<table border = 1><tr> <th> Column Name </th> <th> Column Type </th> <th> Null/Not Null</th> <th> Primary Key </th>";
-            $q = "EXPLAIN t5_customer";
-            $r = mysqli_query ($dbc,$q);
-            if($r ){
-                while ($row= mysqli_fetch_array($r, MYSQLI_NUM)){
-                    echo "<tr><td>". $row[0] . "</td><td>". $row[1]. "</td><td>". $row[2]. "</td><td>". $row[3]. "</td></tr>";
-                }
-            }
-            echo "</table>";
-            echo "<hr>";
 
-            echo "<h3> Orders table explained </h3>";
-            echo "<table border = 1><tr> <th> Column Name </th> <th> Column Type </th> <th> Null/Not Null</th> <th> Primary Key </th>";
-            $q = "EXPLAIN t5_orders";
-            $r = mysqli_query ($dbc,$q);
-            if($r ){
-                while ($row= mysqli_fetch_array($r, MYSQLI_NUM)){
-                    echo "<tr><td>". $row[0] . "</td><td>". $row[1]. "</td><td>". $row[2]."</td><td>". $row[3]. "</td></tr>";
-                }
-            }
-            echo "</table>";
-            echo "<hr>";
-           
-
-            echo "<h3> Products table explained </h3>";
-            echo "<table border = 1><tr> <th> Column Name </th> <th> Column Type </th> <th> Null/Not Null</th> <th> Primary Key </th>";
-            $q = "EXPLAIN t5_product";
-            $r = mysqli_query ($dbc,$q);
-            if($r ){
-                while ($row= mysqli_fetch_array($r, MYSQLI_NUM)){
-                    echo "<tr><td>". $row[0] . "</td><td>". $row[1]. "</td><td>". $row[2]."</td><td>". $row[3]. "</td></tr>";
-                }
-            }
-            echo "</table>";
-            echo "<hr>";
+            echo "<h3> Choose a table to view its explanation </h3>";
+            echo "<form action = '' method = 'POST'>";
+            echo "<input type = 'radio' name = 'sort' value = 'customers'> Customers Table";
+            echo "<br><input type = 'radio' name = 'sort' value = 'orders'> Orders Table";
+            echo "<br><input type = 'radio' name = 'sort' value = 'products'> Products Table";
+            echo "<br><input type = 'radio' name = 'sort' value = 'suppliers'> Suppliers Table";
+            echo "<br><input type = 'radio' name = 'sort' value = 'users'> Users Table";
+            echo "<br>";
+            echo "<br> <input type = 'submit' value = 'Explain' style = 'background-color:rgb(220, 26, 34); color:white; height:40px; width:150px;font-family: Monaco; font-size: 25px;'>";
+            echo "</form>";
+            echo "<br>";
             
-
-            echo "<h3> Suppliers table explained </h3>"; 
-            echo "<table border = 1><tr> <th> Column Name </th> <th> Column Type </th> <th> Null/Not Null</th> <th> Primary Key </th>";
-            $q = "EXPLAIN t5_supplier";
-            $r = mysqli_query ($dbc,$q);
-            if($r ){
-                while ($row= mysqli_fetch_array($r, MYSQLI_NUM)){
-                    echo "<tr><td>". $row[0] . "</td><td>". $row[1]. "</td><td>". $row[2]. "</td><td>". $row[3]. "</td></tr>";
+            if (isset($_POST['sort'])){
+                if ($_POST['sort']=='customers'){
+                    echo "<hr>";
+                    echo "<h1 class='redcolor'> CUSTOMERS </h1>";
+                    echo "<table border = 1 class = 'tableoutline'><tr> <th class='tableheader'> Column Name </th> <th class='tableheader'> Column Type </th> <th class='tableheader'> Null/Not Null</th> <th class='tableheader'> Primary Key </th>";
+                    $q = "EXPLAIN t5_customer";
+                    $r = mysqli_query ($dbc,$q);
+                    if($r ){
+                        while ($row= mysqli_fetch_array($r, MYSQLI_NUM)){
+                            echo "<tr><td>". $row[0] . "</td><td>". $row[1]. "</td><td>". $row[2]. "</td><td>". $row[3]. "</td></tr>";
+                        }
+                    }
+                    echo "</table>";
                 }
-            }
-            echo "</table>";
-            echo "<hr>";
+                if ($_POST['sort']=='orders'){
+                    echo "<hr>";
+                    echo "<h1 class='redcolor'> ORDERS </h1>";
+                    echo "<table border = 1 class = 'tableoutline'><tr> <th class='tableheader'> Column Name </th> <th class='tableheader'> Column Type </th> <th class='tableheader'> Null/Not Null</th> <th class='tableheader'> Primary Key </th>";
+                    $q = "EXPLAIN t5_orders";
+                    $r = mysqli_query ($dbc,$q);
+                    if($r ){
+                        while ($row= mysqli_fetch_array($r, MYSQLI_NUM)){
+                            echo "<tr><td>". $row[0] . "</td><td>". $row[1]. "</td><td>". $row[2]."</td><td>". $row[3]. "</td></tr>";
+                        }
+                    }
+                    echo "</table>";
+                }
             
-            echo "<h3> Users table explained </h3>";
-            echo "<table border = 1><tr> <th> Column Name </th> <th> Column Type </th> <th> Null/Not Null</th> <th> Primary Key </th>";
-            $q = "EXPLAIN t5_user";
-            $r = mysqli_query ($dbc,$q);
-            if($r ){
-                while ($row= mysqli_fetch_array($r, MYSQLI_NUM)){
-                    echo "<tr><td>". $row[0] . "</td><td>". $row[1]. "</td><td>". $row[2]. "</td><td>". $row[3]. "</td></tr>";
+                if ($_POST['sort']=='products'){
+                    echo "<hr>";
+                    echo "<h1 class='redcolor'> PRODUCTS </h1>";
+                    echo "<table border = 1 class = 'tableoutline'><tr> <th class='tableheader'> Column Name </th> <th class='tableheader'> Column Type </th> <th class='tableheader'> Null/Not Null</th> <th class='tableheader'> Primary Key </th>";
+                    $q = "EXPLAIN t5_product";
+                    $r = mysqli_query ($dbc,$q);
+                    if($r ){
+                        while ($row= mysqli_fetch_array($r, MYSQLI_NUM)){
+                            echo "<tr><td>". $row[0] . "</td><td>". $row[1]. "</td><td>". $row[2]."</td><td>". $row[3]. "</td></tr>";
+                        }
+                    }
+                    echo "</table>";
                 }
-            }
-            echo "</table>";
+                if ($_POST['sort']=='suppliers'){
+                    echo "<hr>";
+                    echo "<h1 class='redcolor'> SUPPLIERS </h1>"; 
+                    echo "<table border = 1 class = 'tableoutline'><tr> <th class='tableheader'> Column Name </th> <th class='tableheader'> Column Type </th> <th class='tableheader'> Null/Not Null</th> <th class='tableheader'> Primary Key </th>";
+                    $q = "EXPLAIN t5_supplier";
+                    $r = mysqli_query ($dbc,$q);
+                    if($r ){
+                        while ($row= mysqli_fetch_array($r, MYSQLI_NUM)){
+                            echo "<tr><td>". $row[0] . "</td><td>". $row[1]. "</td><td>". $row[2]. "</td><td>". $row[3]. "</td></tr>";
+                        }
+                    }
+                    echo "</table>";
+                }
+                if ($_POST['sort']=='users'){
+                    echo "<hr>";
+                    echo "<h1 class='redcolor'> USERS </h1>";
+                    echo "<table border = 1 class = 'tableoutline'><tr> <th class='tableheader'> Column Name </th> <th class='tableheader'> Column Type </th> <th class='tableheader'> Null/Not Null</th> <th class='tableheader'> Primary Key </th>";
+                    $q = "EXPLAIN t5_user";
+                    $r = mysqli_query ($dbc,$q);
+                    if($r ){
+                        while ($row= mysqli_fetch_array($r, MYSQLI_NUM)){
+                            echo "<tr><td>". $row[0] . "</td><td>". $row[1]. "</td><td>". $row[2]. "</td><td>". $row[3]. "</td></tr>";
+                        }
+                    }
+                    echo "</table>";
+                }
+
+            }  
             echo "<hr>";
             include "file_author.php";
     ?>
