463a09b1f2a6e2731b6ac7240c7c37c377562283
MaeveLonergan <98499704+MaeveLonergan@users.noreply.github.com>
2022-12-01 18:11:24 -0500

    Update user.php

--- user.php
+++ user.php
@@ -15,7 +15,12 @@
             h3 { color:rgb(220, 26, 34);font-size: 25px;font-family: Monaco;}
             header {background-color:rgb(220, 26, 34);}
             footer {background-color:rgb(245, 255, 245);}
-            th {font-family: Papyrus;}
+            th {font-family: Papyrus; padding: 5px 5px 5px 5px; font-weight:2300; font-size: 25px;}
+            td {padding: 5px 5px 5px 5px;font-size: 25px;} 
+            form { font-family: Monaco;font-size: 15px;}
+           .tableoutline{background-color:rgb(220, 26, 34);}
+           .tableheader{background:rgb(249, 208, 208 );}
+           .redcolor{color:rgb(220, 26, 34);}
         </style>
     </head>
     <body>
@@ -57,34 +62,36 @@
         }
 
 
-        echo "<br><br><h3>Display the User Table </h3>";
+        echo "<h1 class='redcolor'>Users </h1>";
         $q = "Select * from t5_user".  $sort_type . " " . $dir;
 
 
         $r = mysqli_query($dbc, $q);
-        echo " <table border = 2> <tr> <th> Username </th> <th> Password </th> <th> Password reset date </th> <th> Position title </th> <th> Monthly Report </th> <th> Deleted </th> <th> Email </th> <th> Date of last password cange </th> <th> Type of passsword </th>";
+        echo " <table border = 2 class='tableoutline'> <tr> <th class='tableheader'> Username </th> <th class='tableheader'> Password </th> <th class='tableheader'> Password reset date </th> <th class='tableheader'> Position title </th> <th class='tableheader'> Monthly Report </th> <th class='tableheader'> Deleted </th> <th class='tableheader'> Email </th> <th class='tableheader'> Date of last password cange </th> <th class='tableheader'> Type of passsword </th>";
         if($r ){
             while ($row= mysqli_fetch_array($r, MYSQLI_NUM))
             {
-                echo "<tr><td>". $row[0] . "</td><td>". $row[1]. "</td><td>". $row[2]. "</td><td>". $row[3] . "</td><td>". $row[4] ."</td><td>". $row[5] ."</td><td>". $row[6] ."</td><td>". $row[7] ."</td><td>". $row[8] ."</td><td>". $row[9] ."</td></tr>";
+                echo "<tr><td>". $row[0] . "</td><td>". $row[1]. "</td><td>". $row[2]. "</td><td>". $row[3] . "</td><td>". $row[4] ."</td><td>". $row[5] ."</td><td>". $row[6] ."</td><td>". $row[7] ."</td><td>". $row[8] ."</td></tr>";
             }
         }
         echo "</table>";
 
-
+        echo "<br><h1 class='redcolor'>Sort table</h1>";
         echo "<form action = '' method = 'POST'>";
-        echo "<br> <input type = 'submit' value = 'Sort it!' style = 'background-color:rgb(220, 26, 34); color:white;'>";
+        echo "<p class='redcolor'>TABLE COLUMN</p>";
         echo "<input type = 'radio' name = 'sort' value = 'username'>    Username";
-        echo "<input type = 'radio' name = 'sort' value = 'passwrd'>    Password";
-        echo "<input type = 'radio' name = 'sort' value = 'passwrd_reset'>    Password reset date";
-        echo "<input type = 'radio' name = 'sort' value = 'title'>    Job title";
-        echo "<input type = 'radio' name = 'sort' value = 'monthly_report'>    Monthly Report";
-        echo "<input type = 'radio' name = 'sort' value = 'deleted'>    Deleted";
-        echo "<input type = 'radio' name = 'sort' value = 'email'>    Email";
-        echo "<input type = 'radio' name = 'sort' value = 'lastchanged'>    Date of last password change";
-        echo "<input type = 'radio' name = 'sort' value = 'password_type'>    Type of password";
-        echo "<br><input type = 'radio' name = 'direction' value = 'ASC'>    Ascending";
-        echo "<input type = 'radio' name = 'direction' value = 'DESC'>    Descending";
+        echo "<br><input type = 'radio' name = 'sort' value = 'passwrd'>    Password";
+        echo "<br><input type = 'radio' name = 'sort' value = 'passwrd_reset'>    Password reset date";
+        echo "<br><input type = 'radio' name = 'sort' value = 'title'>    Job title";
+        echo "<br><input type = 'radio' name = 'sort' value = 'monthly_report'>    Monthly Report";
+        echo "<br><input type = 'radio' name = 'sort' value = 'deleted'>    Deleted";
+        echo "<br><input type = 'radio' name = 'sort' value = 'email'>    Email";
+        echo "<br><input type = 'radio' name = 'sort' value = 'lastchanged'>    Date of last password change";
+        echo "<br><input type = 'radio' name = 'sort' value = 'password_type'>    Type of password";
+        echo "<p class='redcolor'>DIRECTION</p>";
+        echo "<input type = 'radio' name = 'direction' value = 'ASC'>    Ascending";
+        echo "<br><input type = 'radio' name = 'direction' value = 'DESC'>    Descending";
+        echo "<br><br> <input type = 'submit' value = 'Sort it!' style = 'background-color:rgb(220, 26, 34); color:white;font-family: Monaco;font-size: 15px; '>";
         echo "</form>";
         include "file_author.php";
         ?>
