3580ba3d8a674d80090964a47ccd3e23f498cd5b
MaeveLonergan <98499704+MaeveLonergan@users.noreply.github.com>
2022-12-11 17:32:43 -0500

    Update tables.php

--- tables.php
+++ tables.php
@@ -1,12 +1,12 @@
 <!DOCTYPE html>
-<!--    t5homepage.html   Homepage
+<!--    t5homepage.html   Tables page
     09/21/2022 t5 Original Program
 -->
 <html lang="en">
 
     <head>
         <title>
-            Stellar Sneakers Admin
+            View Tables
         </title>
         <meta charset="utf-8">
         <style>
@@ -24,14 +24,14 @@
         <center><img src="logo.png"; width = 1100px; height = 250px></center>
         
     </header>
-    <!-- Will become links to navigate the website-->
+    <!-- Links to navigate the website-->
     <main>
         <br>
         <center><table border=0></center>
         <table style = "border-collapse: separate; border-spacing: 150px 0;">
             <tr>
-            <th><a class= "active" href="index.php"> Home</a></th>
-            <th><a class= "active" href="tables.php"> Tables</a></th>
+                <th><a class= "active" href="index.php"> Home</a></th>
+                <th><a class= "active" href="tables.php"> Tables</a></th>
                 <th><a class= "active" href="contact.php"> Contact</a></th>
                 <th><a class= "active" href="admin.php"> Admin</a></th>
                 <th><a class= "active" href="login.php"> Login</a></th>
@@ -40,11 +40,12 @@
         </table>
         <br>
         
-        <!-- start of the new code -->
+        
         <?php
         session_start();
         $FILE_AUTHOR="Maeve Lonergan";
 
+        //initialize session variables
         if (isset($_SESSION['login_status'])){
             $login_status = $_SESSION['login_status'];
           } else{
@@ -52,10 +53,18 @@
           }
             echo "<br>" . $login_status;
 
+        //if logged in, you can choose which table to view
         if (isset($_SESSION['login_status'])){
-            echo"<h1><a class= 'active' href='product.php'> Products</a></h1>";
-            echo"<h1><a class= 'active' href='sales.php'> Suppliers</a></h1>";
-            echo"<h1><a class= 'active' href='customer.php'> Customers</a></h1>";
+            echo "<form action='product.php' method = 'POST'>";
+            echo "<input type='submit' value='Products Table' name='submit' style='background-color:rgb(220, 26, 34); color:white;font-family: Monaco;font-size: 55px;'>";
+            echo "</form>";
+            echo "<form action='suppliers.php' method = 'POST'>";
+            echo "<input type='submit' value='Suppliers Table' name='submit' style='background-color:rgb(220, 26, 34); color:white;font-family: Monaco;font-size: 55px;'>";
+            echo "</form>";
+            echo "<form action='customer.php' method = 'POST'>";
+            echo "<input type='submit' value='Customers Table' name='submit' style='background-color:rgb(220, 26, 34); color:white;font-family: Monaco;font-size: 55px;'>";
+            echo "</form>";
+        //prompt for login
         } else{
             echo "<br> You have not logged in yet!";
             echo "<br> Please log in at link below in order to change tables or add to the tables!";
@@ -65,6 +74,8 @@
              <th><a class= 'active' href='login.php'> Login</a></th>
          </tr>
         </table>";
+
+        include "footer.php";
        
         ?>
     </main>
