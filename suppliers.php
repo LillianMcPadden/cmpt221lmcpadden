4d091d1b8beb3676ccb0a612bed476230685f073
MaeveLonergan <98499704+MaeveLonergan@users.noreply.github.com>
2022-12-11 17:30:11 -0500

    Update and rename sales.php to suppliers.php

--- suppliers.php
+++ suppliers.php
@@ -1,12 +1,12 @@
 <!DOCTYPE html>
-<!--    t5homepage.html   Homepage
+<!--    t5homepage.html   Suppliers Table
     09/21/2022 t5 Original Program
 -->
 <html lang="en">
 
     <head>
         <title>
-            Stellar Sneakers Homepage
+            Suppliers
         </title>
         <meta charset="utf-8">
         <style>
@@ -15,12 +15,14 @@
             h3 { color:rgb(220, 26, 34);font-size: 25px;font-family: Monaco;}
             header {background-color:rgb(220, 26, 34);}
             footer {background-color:rgb(245, 255, 245);}
-           th {font-family: Papyrus; padding: 5px 5px 5px 5px; font-weight:2300; font-size: 25px;}
+            th {font-family: Papyrus; padding: 5px 5px 5px 5px; font-weight:2300; font-size: 25px;}
             td {padding: 5px 5px 5px 5px;font-size: 25px;} 
-            form { font-family: Monaco;font-size: 25px;}
-           .tableoutline{background-color:rgb(220, 26, 34);}
-           .tableheader{background:rgb(249, 208, 208 );}
-           .redcolor{color:rgb(220, 26, 34);}
+            form { font-family: Monaco;font-size: 15px;}
+           .tableoutline {background-color:rgb(220, 26, 34);}
+           .tableheader {background:rgb(249, 208, 208 );}
+           .redcolor {color:rgb(220, 26, 34);}
+           .test {font-family: Papyrus; font-size:16px; padding: 0px 0px 0px 0px;  }
+           
         </style>
     </head>
     <body>
@@ -29,30 +31,29 @@
         <center><img src="logo.png"; width = 1100px; height = 250px></center>
         
     </header>
-    <!-- Will become links to navigate the website-->
+    <!-- Links to navigate the website-->
     <main>
         <br>
         <center><table border=0></center>
         <table style = "border-collapse: separate; border-spacing: 150px 0;">
             <tr>
-                <th><a class= "active" href="index.php"> Home</a></th>
-                <th><a class= "active" href="tables.php"> Tables</a></th>
-                <th><a class= "active" href="contact.php"> Contact</a></th>
-                <th><a class= "active" href="admin.php"> Admin</a></th>
-                <th><a class= "active" href="login.php"> Login</a></th>
-                <th><a class= "active" href="logout.php"> Logout</a></th>
+                <th class='test'><a class= "active" href="index.php"> Home</a></th>
+                <th class='test'><a class= "active" href="tables.php"> Tables</a></th>
+                <th class='test'><a class= "active" href="contact.php"> Contact</a></th>
+                <th class='test'><a class= "active" href="admin.php"> Admin</a></th>
+                <th class='test'><a class= "active" href="login.php"> Login</a></th>
+                <th class='test'><a class= "active" href="logout.php"> Logout</a></th>
             </tr>
         </table>
         <br>
         
-        <!-- start of the new code -->
+       
         <?php
         $FILE_AUTHOR="Veronica Longley";
 
         require "connect_db.php";
 
-            
-        #initialize sort and direction to hold how the user wants to display table 
+         #initialize sort and direction to hold how the user wants to display table 
         if(ISSET($_POST['sort'])){
             $sort_type = " ORDER BY ". $_POST['sort'];
         }
@@ -68,11 +69,10 @@
 
 
         echo "<br><br><h1 class='redcolor'>Display the Supplier Table </h1>";
-            
         # set query based on form input from user 
         $q = "Select * from t5_supplier".  $sort_type . " " . $dir;
 
-        # print table row by row 
+         # print table row by row 
         $r = mysqli_query($dbc, $q);
         echo " <table border = 1 class = 'tableoutline'> <tr> <th class='tableheader'> Supplier ID </th> <th class='tableheader'> Supplier Name </th> <th class='tableheader'> Supplier Address </th> <th class='tableheader'> Number of Sales </th> <th class='tableheader'> Supplier Email </th> <th class='tableheader'> Supplier Phone Number </th><th class='tableheader'> Deleted </th>";
         if($r ){
@@ -84,8 +84,9 @@
         echo "</table>";
 
         # form to allow user to select how to sort the table 
+        echo "<h1 class='redcolor'>Sort table</h1>";
         echo "<form action = '' method = 'POST'>";
-        echo "<br> <input type = 'submit' value = 'Sort it!' style = 'background-color:rgb(220, 26, 34); color:white;'>";
+        echo "<p class='redcolor'>TABLE COLUMN</p>";
         echo "<input type = 'radio' name = 'sort' value = 'supplier_id'>    ID";
         echo "<input type = 'radio' name = 'sort' value = 'supName'>    Name";
         echo "<input type = 'radio' name = 'sort' value = 'supAddress'>    Address";
@@ -93,8 +94,15 @@
         echo "<input type = 'radio' name = 'sort' value = 'supEmail'>    Email";
         echo "<input type = 'radio' name = 'sort' value = 'supPhone_num'>    Phone Number";
         echo "<input type = 'radio' name = 'sort' value = 'deleted'>    Deleted";
-        echo "<br><input type = 'radio' name = 'direction' value = 'ASC'>    Ascending";
+        echo "<br><p class='redcolor'>DIRECTION</p>";
+        echo "<input type = 'radio' name = 'direction' value = 'ASC'>    Ascending";
         echo "<input type = 'radio' name = 'direction' value = 'DESC'>    Descending";
+        echo "<br><br> <input type = 'submit' value = 'Sort it!' style = 'background-color:rgb(220, 26, 34); color:white;font-family: Monaco;font-size: 15px; '>";
+        echo "</form>";
+
+        //button to add row
+        echo "<form action='add_supplier.php' method = 'POST'>";
+        echo "<br><br> <input type='submit' value='Add a Row' name='submit' style='background-color:rgb(220, 26, 34); color:white;font-family: Monaco;font-size: 45px;'>";
         echo "</form>";
         include "file_author.php";
         ?>
